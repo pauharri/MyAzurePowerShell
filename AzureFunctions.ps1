b1976b775c42c00a4762c2d5b9c3c1be4ee09bb0
pauharri <66027941+pauharri@users.noreply.github.com>
2020-09-02 16:17:57 -0400

    Update AzureFunctions.ps1

--- AzureFunctions.ps1
+++ AzureFunctions.ps1
@@ -56,3 +56,16 @@ Function Get-UnusedDisks{
         return $MyScriptBlock.Invoke()
     }
 }
+
+
+Function Get-DBAllocations{
+    param(
+        [Switch]$AllSubscriptions
+    )
+    $MyScriptBlock = [scriptblock]::Create('Get-AzSqlServer | Get-AzSqlDatabase | select @{N="Subscription";E={(Get-AzContext).Subscription.Name}}, ResourceGroupName, ServerName, DatabaseName, DatabaseId, CurrentServiceObjectiveName, Capacity, Family, SkuName, LicenseType, Location, ZoneRedundant, @{N="MaxCPU";E={((Get-AzMetric -WarningAction 0 -ResourceId $_.ResourceId -MetricName cpu_percent -TimeGrain 01:00:00 -StartTime ((Get-Date).AddDays(-14)) -EndTime (Get-Date) -AggregationType Maximum | select -ExpandProperty Data).maximum | measure -Maximum).Maximum}}')
+    If($AllSubscriptions){
+        return Invoke-AllSubs -ScriptBlock $MyScriptBlock
+    }Else{
+        return $MyScriptBlock.Invoke()
+    }
+}
