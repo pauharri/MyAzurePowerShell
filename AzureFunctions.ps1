29f9de4f4b0b8bd9565ecf9b04085dabbf6eb406
pauharri <66027941+pauharri@users.noreply.github.com>
2020-09-08 11:26:33 -0400

    Update AzureFunctions.ps1

--- AzureFunctions.ps1
+++ AzureFunctions.ps1
@@ -141,3 +141,58 @@ Function New-Route {
         }
     }
 }
+
+
+function Get-ExtraGBPaidFor {
+    [CmdletBinding()]
+    param (
+        [Switch]
+        $AllSubscriptions,
+    
+        [Parameter(ValueFromPipeline = $true)]
+        $Subscription
+    )
+
+    begin {
+        Function Get-ExtraGBPaidForHelper{
+            param(
+                $disk
+            )
+            $PList = @(4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32767) #premium ssd
+            $EList = @(4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32767) #standard ssd
+            $SList = @(32,64,128,256,512,1024,2048,4096,8192,16384,32767) #standard hdd
+        
+            If($($disk.sku.Name) -like "*UltraSSD*"){
+                0
+            }
+            ElseIf($($disk.sku.Name) -like "*Premium*"){
+                $allowedDiskSizes = $PList
+            }Elseif($($disk.sku.Name) -like "*StandardSSD*"){
+                $allowedDiskSizes = $EList
+            }Elseif($($disk.sku.Name) -like "*Standard*"){
+                $allowedDiskSizes = $SList
+            }
+        
+            If($allowedDiskSizes -contains $($disk.diskSizeGB)){
+                0
+            }
+            Elseif($($disk.diskSizeGB) -gt $($allowedDiskSizes[$allowedDiskSizes.Count - 1])){
+                Write-Error "Disk size too big"
+            }
+            Else{
+                For($i = 0;$i -lt $($allowedDiskSizes.Count -1);$i++){
+                    If(($($disk.diskSizeGB) -gt $allowedDiskSizes[$i]) -and ($($disk.diskSizeGB) -lt $allowedDiskSizes[($i+1)])){
+                        $allowedDiskSizes[$i+1]-$($disk.diskSizeGB)
+                    }
+                }
+            }
+        }
+        $MyScriptBlock = {
+            Get-AzDisk | select @{ N = "Subscription"; E = { (Get-AzContext).Subscription.Name } },ResourceGroupName, Name, Id, OsType, DiskSizeGB, @{N="ExtraGBPaidFor";E={Get-ExtraGBPaidForHelper -disk $_}}
+        }        
+    }
+    process {
+        if ($Subscription) { $Subscription | Invoke-AzureCommand -ScriptBlock $MyScriptBlock }
+        else { Invoke-AzureCommand -ScriptBlock $MyScriptBlock -AllSubscriptions:$AllSubscriptions }
+    }
+}
