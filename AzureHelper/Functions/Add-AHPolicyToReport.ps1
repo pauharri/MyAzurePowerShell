1b4299fc5a8db9a100222b73e58e4aae85eb0516
pauharri <66027941+pauharri@users.noreply.github.com>
2020-09-14 09:57:22 -0400

    Update Add-AHPolicyToReport.ps1

--- AzureHelper/Functions/Add-AHPolicyToReport.ps1
+++ AzureHelper/Functions/Add-AHPolicyToReport.ps1
@@ -1,4 +1,33 @@
 Function Add-AHPolicyToReport {
+    <#
+.SYNOPSIS
+    Adds an Azure policy to the list of policies to be analyzed.
+.DESCRIPTION
+    Add-AHPolicyToReport adds an Azure Policy to the list of policies
+    to be analyzed by other AzureHelper cmdlets.
+.PARAMETER PolicyDefinitionID
+    Define the policy to be added by the PolicyDefinitionID
+.PARAMETER GUI
+    Use the GUI switch to use the GUI to select the PolicyDefinitions to add
+.EXAMPLE
+    Add-AHPolicyToReport -PolicyDefinitionID 
+.EXAMPLE
+    Get-NonCompliantResources -AllSubscriptions | Export-Csv NonCompliantResources-Policy1.csv -NoTypeInformation
+.EXAMPLE
+    Get-NonCompliantResources -AllSubscriptions -PolicyDefinitionID '/providers/Microsoft.Authorization/policyDefinitions/34c877ad-507e-4c82-993e-3452a6e0ad3c' | Export-Csv .\StorageAccountsShouldRestrictNetworkAccess2.csv -NoTypeInformation
+.INPUTS
+    String
+.OUTPUTS
+    Selected.Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource
+.NOTES
+    Author:  Paul Harrison
+    .LINK
+    Get-AHSecurityReport
+    Add-AHPolicyToReport
+    Get-AHPolicyToReport
+    Remove-AHPolicyToReport
+    Get-AHComplianceReport
+#>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
