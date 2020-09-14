11c9c652aba74df16d368cb285e4fab89d46c1a5
pauharri <66027941+pauharri@users.noreply.github.com>
2020-09-14 09:03:05 -0400

    Delete Select-AHSubscription.ps1

--- Select-AHSubscription.ps1
+++
@@ -1,8 +0,0 @@
-Function Select-AHSubscription {
-    $sub = $Null
-    While ($Null -eq $sub -or $sub -is [array]) {
-        $sub = (Get-AzSubscription | Select-Object Name, Id | Out-GridView -PassThru -Title "Select the subscription to use")
-    }
-    try { Set-AzContext $($sub.id) }
-    catch { throw }
-}
