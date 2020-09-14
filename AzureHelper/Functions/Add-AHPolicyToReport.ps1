35919445f3dc4bd3f8259a12c23e834fc7e03914
pauharri <66027941+pauharri@users.noreply.github.com>
2020-09-14 09:20:01 -0400

    Update Add-AHPolicyToReport.ps1

--- AzureHelper/Functions/Add-AHPolicyToReport.ps1
+++ AzureHelper/Functions/Add-AHPolicyToReport.ps1
@@ -2,17 +2,26 @@ Function Add-AHPolicyToReport {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string]
-        $PolicyDefinitionID
+        [string[]]
+        $PolicyDefinitionID,
+        
+        [switch]
+        $GUI
     )
-    If ($Null -eq $PolicyDefinitionID -or (Get-AzPolicyDefinition -Id $PolicyDefinitionID) -is [array]) { 
-        #If a PolicyDefinitionID is passed at the CLI and is malformed then this will return an array and re-prompt the user for a correct value
-        throw { "Invalid PolicyDefinitionID" }
+    If ($GUI) {
+        (Get-AzPolicyDefinition | Select-Object * -ExpandProperty Properties | Out-GridView -PassThru -Title "Select the policies to add to your report").PolicyDefinitionId | Add-AHPolicyToReport
     }
-    Elseif ($Script:PolicyDefinitionIDs -contains $PolicyDefinitionID) {
-        Throw { "The PolicyDefinitionID $PolicyDefinitionID is already in the list." }
-    }
-    Else {
-        $Script:PolicyDefinitionIDs += $PolicyDefinitionID
+
+    ForEach ($ID in $PolicyDefinitionID) {
+        If ($Null -eq $PolicyDefinitionID -or (Get-AzPolicyDefinition -Id $PolicyDefinitionID) -is [array]) { 
+            #If a PolicyDefinitionID is passed at the CLI and is malformed then this will return an array and re-prompt the user for a correct value
+            throw { "Invalid PolicyDefinitionID" }
+        }
+        Elseif ($Script:PolicyDefinitionIDs -contains $PolicyDefinitionID) {
+            Throw { "The PolicyDefinitionID $PolicyDefinitionID is already in the list." }
+        }
+        Else {
+            $Script:PolicyDefinitionIDs += $PolicyDefinitionID
+        }
     }
 }
